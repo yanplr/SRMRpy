4313db388893cc81ac182482776e7da74c4d879d
yanplr <857103939@qq.com>
2022-11-02 11:11:15 +0800

    Update setup.py
    add ghproxy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires = [
         'numpy',
         'scipy',
-        'Gammatone @ https://github.com/detly/gammatone/archive/master.zip#egg=Gammatone',
+        'Gammatone @ https://gitproxy.com/https://github.com/detly/gammatone/archive/master.zip#egg=Gammatone',
     ],
 
     tests_require = [
